13ae23649ba14aebcce99b16a544cd1136ead6da
Silson sapkota <silson007@gmail.com>
2023-08-04 02:07:49 +0545

    ```chore:``` remove unused dependencies

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  webpack: (config, options) => {
-    // Important: return the modified config
-    config.module.rules.push({
-      test: /\.node/,
-      use: 'raw-loader',
-    })
-    return config
-  },
   images: {
     domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
   },
