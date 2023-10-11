be0590c31027dde33237b6125620683b8f1d7185
Silson sapkota <silson007@gmail.com>
2023-10-12 02:03:42 +0545

    ```fix:``` update shadow preview to #000

--- utils/config.ts
+++ utils/config.ts
@@ -165,31 +165,31 @@ export const shadows: Shadow[] = [
     name: 'bottom',
     fullName: 'Bottom',
     shadow: `0 25px 50px -12px`,
-    preview: `0 25px 50px -12px #00000065`,
+    preview: `0 25px 50px -12px #000000`,
   },
   {
     name: 'sm',
     fullName: 'Small',
     shadow: `0px 3px 8px 0px`,
-    preview: `0px 3px 8px 0px #00000065`,
+    preview: `0px 3px 8px 0px #000000`,
   },
   {
     name: 'md',
     fullName: 'Medium',
     shadow: `0px 6px 20px`,
-    preview: `0px 6px 20px #00000065`,
+    preview: `0px 6px 20px #000000`,
   },
   {
     name: 'lg',
     fullName: 'Large',
     shadow: `0px 8px 60px 0px`,
-    preview: `0px 6px 30px 0px #00000065`,
+    preview: `0px 6px 30px 0px #000000`,
   },
   {
     name: 'x-lg',
     fullName: 'X-Large',
     shadow: `0px 19px 60px 10px`,
-    preview: `0px 10px 60px 0px #00000065`,
+    preview: `0px 10px 60px 0px #000000`,
   },
 ]
 
