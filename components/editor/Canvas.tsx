4ad36bacc2841999a59a5c61c7b356e570fce7c2
Silson sapkota <silson007@gmail.com>
2023-12-31 21:29:45 +0545

    ```fix:``` remove min-height

--- components/editor/Canvas.tsx
+++ components/editor/Canvas.tsx
@@ -278,14 +278,14 @@ export default function Canvas() {
       <section
         ref={parentRef}
         className={`relative flex h-full w-full flex-col overflow-hidden bg-[#111] md:grid md:place-items-center ${
-          aspectRatio <= 1 ? 'p-4 md:p-12' : 'p-4 md:p-12'
+          aspectRatio <= 1 ? 'p-8 md:p-16' : 'p-8 md:p-16'
         }
         `}
         style={parentScaleStyle}
         onWheel={handleScroll}
       >
         <div
-          className={`canvas-container relative flex min-h-[20rem] items-center justify-center overflow-hidden ${
+          className={`canvas-container relative flex items-center justify-center overflow-hidden ${
             aspectRatio <= 1
               ? 'h-auto w-full lg:h-full lg:w-auto'
               : 'h-auto w-full'
