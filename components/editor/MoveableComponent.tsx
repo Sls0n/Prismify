5fd9925b13426d07939904f30bc564f2c31b21ac
Silson sapkota <silson007@gmail.com>
2024-01-02 15:39:08 +0545

    ```fix:``` preserve state after drag, rotate, scale

--- components/editor/MoveableComponent.tsx
+++ components/editor/MoveableComponent.tsx
@@ -11,10 +11,6 @@ import {
   Draggable,
   DraggableProps,
   GroupableProps,
-  OnDragEnd,
-  OnRenderGroupEnd,
-  OnRotateEnd,
-  OnScaleEnd,
   Rotatable,
   RotatableProps,
   Scalable,
@@ -42,101 +38,6 @@ export default function MoveableComponent({ id }: { id: string }) {
   const { width, height } = splitWidthHeight(exactDomResolution)
   const { isMultipleTargetSelected } = useMoveable()
 
-  const handleDrag = (e: OnDragEnd) => {
-    // e.target.style.transform = e?.lastEvent.style.transform
-    if (e?.lastEvent?.translate) {
-      console.log(e)
-      selectedImage &&
-        setImages(
-          images.map((image, index) =>
-            index === selectedImage - 1
-              ? {
-                  ...image,
-                  style: {
-                    ...image.style,
-                    translateX: e?.lastEvent?.translate[0],
-                    translateY: e?.lastEvent?.translate[1],
-                  },
-                }
-              : image
-          )
-        )
-    }
-  }
-
-  const handleRotate = (e: OnRotateEnd) => {
-    selectedImage &&
-      setImages(
-        images.map((image, index) =>
-          index === selectedImage - 1
-            ? {
-                ...image,
-                style: {
-                  ...image.style,
-                  rotate: e?.lastEvent?.rotate,
-                },
-              }
-            : image
-        )
-      )
-  }
-
-  const handleScale = (e: OnScaleEnd) => {
-    console.log(e)
-    selectedImage &&
-      setImages(
-        images.map((image, index) =>
-          index === selectedImage - 1
-            ? {
-                ...image,
-                style: {
-                  ...image.style,
-                  imageSize: `${e.lastEvent?.scale[0]}`,
-                  translateX: e?.lastEvent?.drag?.translate[0],
-                  translateY: e?.lastEvent?.drag.translate[1],
-                },
-              }
-            : image
-        )
-      )
-  }
-
-  const handleRenderGroupEnd = (e: OnRenderGroupEnd) => {
-    if (isMultipleTargetSelected) {
-      const updatedImages = images.map((image, index) => {
-        const targetIndex = e.events.findIndex(
-          (ev: any) => ev.target.id === `${image.id}`
-        )
-        const updatedEvent = e.events[targetIndex]
-
-        if (targetIndex !== -1) {
-          const updatedEvent = e.events[targetIndex]
-
-          console.log(updatedEvent)
-
-          return {
-            ...image,
-            style: {
-              ...image.style,
-              translateX: updatedEvent.transformObject.translate[0],
-              translateY: updatedEvent.transformObject.translate[1],
-              // rotate: updatedEvent.transformObject.rotate,
-              imageSize: `${updatedEvent.transformObject.scale[0]}`,
-              // rotateX: updatedEvent.transformObject.rotateX,
-              // rotateY: updatedEvent.transformObject.rotateY,
-              // rotateZ: updatedEvent.transformObject.rotateZ,
-            },
-          }
-        }
-
-        // Return the original image if the target is not found
-        return image
-      })
-
-      setImages(updatedImages)
-    }
-  }
-
   const otherImages = images.filter((image) => image.id !== selectedImage)
   const elementGuidelines = otherImages.map((image) => ({
     element:
@@ -147,155 +48,252 @@ export default function MoveableComponent({ id }: { id: string }) {
 
   const [domWidth, domHeight]: number[] = domResolution.split('x').map(Number)
   return (
-    <>
-      <Moveable
-        ref={moveableRef as any}
-        target={
-          isMultipleTargetSelected
-            ? '.selected'
-            : typeof document !== 'undefined'
-            ? document?.getElementById(id)
-            : ''
-        }
-        hideChildMoveableDefaultLines={true}
-        draggable={true}
-        onDrag={({ target, beforeTranslate }) => {
+    <Moveable
+      ref={moveableRef as any}
+      target={
+        isMultipleTargetSelected
+          ? '.selected'
+          : typeof document !== 'undefined'
+          ? document?.getElementById(id)
+          : ''
+      }
+      hideChildMoveableDefaultLines={true}
+      draggable={true}
+      onDrag={({ target, beforeTranslate }) => {
+        const perspective = target.style.transform.match(/perspective\((.*?)\)/)
+        // @ts-expect-error
+        const xPerc = (beforeTranslate[0] / target?.offsetWidth) * 100
+        // @ts-expect-error
+        const yPerc = (beforeTranslate[1] / target?.offsetHeight) * 100
+
+        const scale = target.style.transform.match(/scale\((.*?)\)/)
+
+        const rotate = target.style.transform.match(/rotate\((.*?)\)/)
+
+        const rotateX = target.style.transform.match(/rotateX\((.*?)\)/)
+        const rotateY = target.style.transform.match(/rotateY\((.*?)\)/)
+        const rotateZ = target.style.transform.match(/rotateZ\((.*?)\)/)
+
+        target.style.transform = `${
+          perspective ? perspective[0] : ''
+        } translate(${xPerc}%, ${yPerc}%) ${scale ? scale[0] : ''} ${
+          rotate ? rotate[0] : ''
+        } ${rotateX ? rotateX[0] : ''} ${rotateY ? rotateY[0] : ''} ${
+          rotateZ ? rotateZ[0] : ''
+        } `
+      }}
+      onDragEnd={({ target, lastEvent }) => {
+        // @ts-expect-error
+        const xPerc = (lastEvent?.translate[0] / target?.offsetWidth) * 100
+        // @ts-expect-error
+        const yPerc = (lastEvent?.translate[1] / target?.offsetHeight) * 100
+
+        selectedImage &&
+          setImages(
+            images.map((image, index) =>
+              index === selectedImage - 1
+                ? {
+                    ...image,
+                    style: {
+                      ...image.style,
+                      translateX: xPerc,
+                      translateY: yPerc,
+                    },
+                  }
+                : image
+            )
+          )
+      }}
+      scalable={true}
+      keepRatio={true}
+      onScale={({ scale, target, drag }) => {
+        const perspective = target.style.transform.match(/perspective\((.*?)\)/)
+        const rotateX = target.style.transform.match(/rotateX\((.*?)\)/)
+
+        const scaleX = scale[0]
+        const scaleY = scale[1]
+
+        const rotate = target.style.transform.match(/rotate\((.*?)\)/)
+
+        const rotateY = target.style.transform.match(/rotateY\((.*?)\)/)
+        const rotateZ = target.style.transform.match(/rotateZ\((.*?)\)/)
+
+        // @ts-expect-error
+        const xPerc = (drag.beforeTranslate[0] / target.offsetWidth) * 100
+        // @ts-expect-error
+        const yPerc = (drag.beforeTranslate[1] / target.offsetHeight) * 100
+
+        target.style.transform = `${
+          perspective ? perspective[0] : ''
+        } translate(${xPerc}%, ${yPerc}%) scale(${scaleX}, ${scaleY}) ${
+          rotate ? rotate[0] : ''
+        } ${rotateX ? rotateX[0] : ''} ${rotateY ? rotateY[0] : ''} ${
+          rotateZ ? rotateZ[0] : ''
+        }`
+      }}
+      onScaleEnd={({ target, lastEvent }) => {
+        const scaleX = lastEvent.scale[0]
+        // @ts-expect-error
+        const xPerc = (lastEvent?.drag?.translate[0] / target.offsetWidth) * 100
+        const yPerc =
+          // @ts-expect-error
+          (lastEvent?.drag?.translate[1] / target.offsetHeight) * 100
+
+        selectedImage &&
+          setImages(
+            images.map((image, index) =>
+              index === selectedImage - 1
+                ? {
+                    ...image,
+                    style: {
+                      ...image.style,
+                      translateX: xPerc,
+                      translateY: yPerc,
+                      imageSize: `${scaleX}`,
+                    },
+                  }
+                : image
+            )
+          )
+      }}
+      rotatable={!isMultipleTargetSelected}
+      rotationPosition={'top'}
+      onRotate={({ target, beforeRotate }) => {
+        const scale = target.style.transform.match(/scale\((.*?)\)/)
+        const translate = target.style.transform.match(/translate\((.*?)\)/)
+
+        const perspective = target.style.transform.match(/perspective\((.*?)\)/)
+        const rotateX = target.style.transform.match(/rotateX\((.*?)\)/)
+        const rotateY = target.style.transform.match(/rotateY\((.*?)\)/)
+        const rotateZ = target.style.transform.match(/rotateZ\((.*?)\)/)
+
+        const rotate = beforeRotate || ''
+
+        target.style.transform = `${perspective ? perspective[0] : ''} ${
+          translate ? translate[0] : ''
+        } ${scale ? scale[0] : ''} ${rotate ? `rotate(${rotate}deg)` : ''} ${
+          rotateX ? rotateX[0] : ''
+        } ${rotateY ? rotateY[0] : ''} ${rotateZ ? rotateZ[0] : ''}`
+      }}
+      onRotateEnd={({ target, lastEvent }) => {
+        const rotate = lastEvent.rotate
+
+        selectedImage &&
+          setImages(
+            images.map((image, index) =>
+              index === selectedImage - 1
+                ? {
+                    ...image,
+                    style: {
+                      ...image.style,
+                      rotate: rotate,
+                    },
+                  }
+                : image
+            )
+          )
+      }}
+      snapRotationThreshold={5}
+      snapRotationDegrees={[0, 90, 180, 270]}
+      snappable={true}
+      snapDirections={{
+        center: true,
+        middle: true,
+        left: true,
+        top: true,
+        right: true,
+        bottom: true,
+      }}
+      snapThreshold={7}
+      horizontalGuidelines={[
+        domHeight / 2 / scaleFactor / quality,
+        domHeight / 1 / scaleFactor / quality,
+        0,
+      ]}
+      verticalGuidelines={[
+        domWidth / 2 / scaleFactor / quality,
+        domWidth / 1 / scaleFactor / quality,
+        0,
+      ]}
+      elementSnapDirections={{
+        top: true,
+        left: true,
+        bottom: true,
+        right: true,
+        center: true,
+        middle: true,
+      }}
+      elementGuidelines={!isMultipleTargetSelected ? elementGuidelines : []}
+      onRenderGroup={({ targets, events }) => {
+        events.forEach((ev) => {
           const perspective =
-            target.style.transform.match(/perspective\((.*?)\)/)
-          // @ts-expect-error
-          const xPerc = (beforeTranslate[0] / target?.offsetWidth) * 100
-          // @ts-expect-error
-          const yPerc = (beforeTranslate[1] / target?.offsetHeight) * 100
+            ev.target.style.transform.match(/perspective\((.*?)\)/)
+          const xPerc =
+            // @ts-expect-error
+            (ev?.transformObject?.translate[0] / ev?.target?.offsetWidth) * 100
+          const yPerc =
+            // @ts-expect-error
+            (ev?.transformObject?.translate[1] / ev?.target?.offsetHeight) * 100
 
-          const scale = target.style.transform.match(/scale\((.*?)\)/)
+          const scale = ev.target.style.transform.match(/scale\((.*?)\)/)
 
-          const rotate = target.style.transform.match(/rotate\((.*?)\)/)
+          const rotate = ev.target.style.transform.match(/rotate\((.*?)\)/)
 
-          const rotateX = target.style.transform.match(/rotateX\((.*?)\)/)
-          const rotateY = target.style.transform.match(/rotateY\((.*?)\)/)
-          const rotateZ = target.style.transform.match(/rotateZ\((.*?)\)/)
+          const rotateX = ev.target.style.transform.match(/rotateX\((.*?)\)/)
+          const rotateY = ev.target.style.transform.match(/rotateY\((.*?)\)/)
+          const rotateZ = ev.target.style.transform.match(/rotateZ\((.*?)\)/)
 
-          target.style.transform = `${
+          ev.target.style.transform = `${
             perspective ? perspective[0] : ''
           } translate(${xPerc}%, ${yPerc}%) ${scale ? scale[0] : ''} ${
             rotate ? rotate[0] : ''
           } ${rotateX ? rotateX[0] : ''} ${rotateY ? rotateY[0] : ''} ${
             rotateZ ? rotateZ[0] : ''
           } `
-        }}
-        // onDragEnd={handleDrag}
-        scalable={true}
-        keepRatio={true}
-        onScale={({ scale, target, drag }) => {
-          const perspective =
-            target.style.transform.match(/perspective\((.*?)\)/)
-          const rotateX = target.style.transform.match(/rotateX\((.*?)\)/)
+        })
+      }}
+      onRenderGroupEnd={({ targets, events }) => {
+        if (isMultipleTargetSelected) {
+          const updatedImages = images.map((image, index) => {
+            const targetIndex = events.findIndex(
+              (ev: any) => ev.target.id === `${image.id}`
+            )
+            const updatedEvent = events[targetIndex]
 
-          const scaleX = scale[0]
-          const scaleY = scale[1]
+            if (targetIndex !== -1) {
+              const updatedEvent = events[targetIndex]
 
-          const rotate = target.style.transform.match(/rotate\((.*?)\)/)
+              console.log(updatedEvent)
 
-          const rotateY = target.style.transform.match(/rotateY\((.*?)\)/)
-          const rotateZ = target.style.transform.match(/rotateZ\((.*?)\)/)
+              const xPerc =
+                (updatedEvent?.transformObject?.translate[0] /
+                  // @ts-expect-error
+                  targets[targetIndex]?.offsetWidth) *
+                100
+              const yPerc =
+                (updatedEvent?.transformObject?.translate[1] /
+                  // @ts-expect-error
+                  targets[targetIndex]?.offsetHeight) *
+                100
 
-          // @ts-expect-error
-          const xPerc = (drag.beforeTranslate[0] / target.offsetWidth) * 100
-          // @ts-expect-error
-          const yPerc = (drag.beforeTranslate[1] / target.offsetHeight) * 100
+              return {
+                ...image,
+                style: {
+                  ...image.style,
+                  translateX: xPerc,
+                  translateY: yPerc,
+                  imageSize: `${updatedEvent.transformObject.scale[0]}`,
+                },
+              }
+            }
 
-          target.style.transform = `${
-            perspective ? perspective[0] : ''
-          } translate(${xPerc}%, ${yPerc}%) scale(${scaleX}, ${scaleY}) ${
-            rotate ? rotate[0] : ''
-          } ${rotateX ? rotateX[0] : ''} ${rotateY ? rotateY[0] : ''} ${
-            rotateZ ? rotateZ[0] : ''
-          }`
-        }}
-        // onScaleEnd={handleScale}
-        rotatable={!isMultipleTargetSelected}
-        rotationPosition={'top'}
-        onRotate={({ target, beforeRotate }) => {
-          const scale = target.style.transform.match(/scale\((.*?)\)/)
-          const translate = target.style.transform.match(/translate\((.*?)\)/)
-
-          const perspective =
-            target.style.transform.match(/perspective\((.*?)\)/)
-          const rotateX = target.style.transform.match(/rotateX\((.*?)\)/)
-          const rotateY = target.style.transform.match(/rotateY\((.*?)\)/)
-          const rotateZ = target.style.transform.match(/rotateZ\((.*?)\)/)
-
-          const rotate = beforeRotate || ''
-
-          target.style.transform = `${perspective ? perspective[0] : ''} ${
-            translate ? translate[0] : ''
-          } ${scale ? scale[0] : ''} ${rotate ? `rotate(${rotate}deg)` : ''} ${
-            rotateX ? rotateX[0] : ''
-          } ${rotateY ? rotateY[0] : ''} ${rotateZ ? rotateZ[0] : ''}`
-        }}
-        // onRotateEnd={handleRotate}
-        snapRotationThreshold={5}
-        snapRotationDegrees={[0, 90, 180, 270]}
-        snappable={true}
-        snapDirections={{
-          center: true,
-          middle: true,
-          left: true,
-          top: true,
-          right: true,
-          bottom: true,
-        }}
-        snapThreshold={7}
-        horizontalGuidelines={[
-          domHeight / 2 / scaleFactor / quality,
-          domHeight / 1 / scaleFactor / quality,
-          0,
-        ]}
-        verticalGuidelines={[
-          domWidth / 2 / scaleFactor / quality,
-          domWidth / 1 / scaleFactor / quality,
-          0,
-        ]}
-        elementSnapDirections={{
-          top: true,
-          left: true,
-          bottom: true,
-          right: true,
-          center: true,
-          middle: true,
-        }}
-        elementGuidelines={!isMultipleTargetSelected ? elementGuidelines : []}
-        onRenderGroup={({ targets, events }) => {
-          events.forEach((ev) => {
-            const perspective =
-              ev.target.style.transform.match(/perspective\((.*?)\)/)
-            const xPerc =
-              // @ts-expect-error
-              (ev?.transformObject?.translate[0] / ev?.target?.offsetWidth) *
-              100
-            const yPerc =
-              // @ts-expect-error
-              (ev?.transformObject?.translate[1] / ev?.target?.offsetHeight) *
-              100
-
-            const scale = ev.target.style.transform.match(/scale\((.*?)\)/)
-
-            const rotate = ev.target.style.transform.match(/rotate\((.*?)\)/)
-
-            const rotateX = ev.target.style.transform.match(/rotateX\((.*?)\)/)
-            const rotateY = ev.target.style.transform.match(/rotateY\((.*?)\)/)
-            const rotateZ = ev.target.style.transform.match(/rotateZ\((.*?)\)/)
-
-            ev.target.style.transform = `${
-              perspective ? perspective[0] : ''
-            } translate(${xPerc}%, ${yPerc}%) ${scale ? scale[0] : ''} ${
-              rotate ? rotate[0] : ''
-            } ${rotateX ? rotateX[0] : ''} ${rotateY ? rotateY[0] : ''} ${
-              rotateZ ? rotateZ[0] : ''
-            } `
+            // Return the original image if the target is not found
+            return image
           })
-        }}
-      />
-    </>
+
+          setImages(updatedImages)
+        }
+      }}
+    />
   )
 }
